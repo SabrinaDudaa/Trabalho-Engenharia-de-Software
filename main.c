56a9fb663f86c577893324afa1a293029e443c5d
Sabrina <114834595+SabrinaDudaa@users.noreply.github.com>
2024-09-19 10:51:26 -0300

    Funções de Organização Produto main.c

--- main.c
+++ main.c
@@ -22,6 +22,24 @@ typedef struct produto{
   int quant;
 }Produto;
 
+
+typedef struct produto{
+  char nome[MAX];
+  char desc [MAX];
+  int id;
+  int quant;
+}Produto;
+
+typedef struct molista{
+  struct produto info;  
+  struct molista *prox;
+}Molista;
+
+typedef struct lista{
+  Molista *cab;
+  Molista *cau;
+}Lista;
+
 void criarVazia(Nolista **l) { *l = NULL; }
 
 int estaVazia(Nolista **l) {
@@ -128,6 +146,18 @@ Produto* novoProduto() {
   return prod;
 }
 
+Produto* editaDesc(){
+  Produto* prod = (Produto*)malloc(sizeof(Produto));
+
+  printf("Digite o ID do Produto:\t");
+  scanf("%d",&prod->id);
+  getchar();
+  printf("Digite a nova descricao do produto:\t");
+  scanf("%49[^\n]",prod->desc);
+
+  return prod;
+}
+
 //Função que vai ser usada no vendaProduto
 Produto* vendProduto(){
   Produto* prod = (Produto*)malloc(sizeof(Produto));
@@ -146,5 +176,82 @@ Produto* vendProduto(){
   else{
     prod->quant = y;
     return prod;
+  }
+}
+
+Produto* leProduto(int id,char nome[MAX],char desc[MAX],int qtde) {
+  Produto* prod = (Produto*)malloc(sizeof(Produto));
+
+  strcpy(prod->nome, nome);
+  strcpy(prod->desc, desc);
+  prod->quant=qtde;
+  prod->id=id;
+
+  return prod;
+}
+
+Lista criaVazia(){
+
+Lista l;
+l.cab = (Molista *)malloc(sizeof(Molista));
+l.cau = (Molista *)malloc(sizeof(Molista));
+
+l.cab->prox = l.cau;
+l.cau->prox = NULL;
+
+return l;
+}
+
+int estarVazia(Lista* l){
+  return (l->cab->prox == l->cau);
+}
+
+
+
+void insereOrdenadoProd(Lista*l, Produto* v){ //vai receber a função novoProduto
+  Molista *ult=l->cab;
+  Molista*novo=(Molista*)malloc(sizeof(Molista));
+
+
+  if(novo!=NULL){
+    novo->info = *v;
+    novo->prox = l->cau;
+
+    for (; ult->prox != l->cau; ult = ult->prox);
+    ult->prox = novo;
+  }
+
+  else{
+    printf("Nao foi possivel alocar");
+  }
+}
+
+
+void imprimeProduto(Lista*l) {
+  Molista*p;
+
+  if(!estarVazia(l)){
+    for(p=l->cab->prox; p!=l->cau; p=p->prox){
+       Produto prod = p->info;
+      printf("%d:\t %s\t -- \t%s. qtde:%d\n",prod.id, prod.nome, prod.desc, prod.quant);
+    }
+  }
+
+  else{
+    printf("\nNenhum produto cadastrado");
+  }
+
+}
+
+void editaDescProduto(Lista*l,Produto*v) {
+  Molista*p;
+
+  for(p=l->cab->prox;p!=l->cau && p->info.id != v->id;p=p->prox);
+
+  if(p==l->cau){
+    printf("Produto nao encontrado.");
+  }
+  else{
+   strcpy(p->info.desc,v->desc);
   }
 }
